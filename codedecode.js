57cff2900f5fd78ecc2a3a11396cf3a633cc35e6
BryantCR <bryancvargas20@gmail.com>
2021-11-24 09:11:40 -0600

    ok

---
+++ codedecode.js
@@ -0,0 +1,20 @@
+/*Build up the caesar( input, shift, encode ) function:
+
+input is a string that refers to the inputted text to be encoded or decoded.
+shift is an integer refers to how much each letter is "shifted" by. 
+  A positive number means shifting to the right (i.e. "A" with a shift of 3 becomes "D") whereas a 
+  negative number means shifting to the left (i.e. "M" with a shift of 3 becomes "K").
+encode is a boolean that refers to whether you should encode or decode the message. 
+  By default, it is set to true.
+
+
+true
+"bad" 
+3
+edg
+
+false
+"edg"
+-3
+bad
+*/
